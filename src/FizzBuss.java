8ac74214b7c68aea5f10babaeeebb6a26e1be55c
Zandros229 <ernest.figura548@gmail.com>
2019-06-25 20:59:15 +0200

    refactor buzz to const

--- src/FizzBuss.java
+++ src/FizzBuss.java
@@ -1,11 +1,12 @@
 public class FizzBuss {
 
     public static final String FIZZ = "fizz";
+    public static final String BUZZ = "buzz";
 
     public String of(int i) {
         if(i==3)
             return FIZZ;
         else
-            return "buzz";
+            return BUZZ;
     }
 }
